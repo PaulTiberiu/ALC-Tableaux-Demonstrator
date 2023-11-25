447c49c644dc5f1940072d78d13ab844e91ec3d0
Iordache Paul-Tiberiu <123265734+PaulTiberiu@users.noreply.github.com>
2023-11-25 13:15:28 +0100

    Update partie1.pl

--- partie1.pl
+++ partie1.pl
@@ -1,23 +1,47 @@
-/*1. Verification de la correction semantique*/
+/*1.---------------------------- Verification de la correction semantique ----------------------------*/
 
-/**a ecrire dans la console pour pouvoir verifier (pas demande mais bon)?----a voir plus tard----**/
-semantique(X) :- setof(X, cnamea(X), ListeConceptsAtomiques).
-semantique(X) :- setof(X, cnamena(X), ListeConceptsNonAtomiques).
-semantique(X) :- setof(X, iname(X), ListeInstances).
-semantique(X) :- setof(X, rname(X), ListeRoles).
 
-/*2. Verification syntaxique*/
+/*Semantique est a vrai si elle verifie que tout identificateur de concept, d'instance, de role en est bien un et qu'ils sont uniques */
+semantique() :- setof(Ca, cnamea(Ca), ListeConceptsAtomiques),
+                setof(Cna, cnamena(Cna), ListeConceptsNonAtomiques),
+                setof(R, rname(R), ListeRoles),
+                setof(I, iname(I), ListeInstances),
+                append(ListeConceptsAtomiques, ListeConceptsNonAtomiques, L1),
+                append(ListeRoles, ListeInstances, L2),
+                append(L1, L2, L), 
+                unique(L).
 
-/*concepts atomiques et non atomiques*/
-concept(X) :- cnamea(X). /**un concept est valide que si il est atomique ou non atomique**/
+
+/* Verifier le cas de base la liste vide est unique ou qu'on test sur un seul element*/
+unique([]).
+/*sinon, faire unique(X|[])*/
+unique([X]).
+/* Verifier l'unicite des elements pour la tete de la liste*/
+unique([X | L]) :-
+    /* Verifier que X n'est pas dans L*/
+    \+ member(X, L), 
+    /*reccursion*/
+    unique(L).
+
+
+/*2.---------------------------- Verification syntaxique ----------------------------*/
+
+
+/*---- Concepts atomiques et non atomiques ----*/
+/*Un concept est valide que s'il est atomique ou non atomique*/
+concept(X) :- cnamea(X).
 concept(X) :- cnamena(X).
+/*Ajouter les identificateurs anything et nothing qui correspondent respectivement a ⊤ et ⊥*/
+cnamea(X) :- nothing(X).
+cnamea(X) :- anything(X).
 
-/*instances */
-instance(I) :- iname(I). /** une instance I est valide que si il existe un identificateur d'instance associé à cette instance **/
+/*Instances*/
+/*Une instance I est valide que s'il existe un identificateur dinstance associe a cette instance*/
+instance(I) :- iname(I).
 
-/*role*/
-role(R) :- rname(R). /** un role R est valide que si il existe un identificateur de role associé à ce role **/
-
+/*Role*/
+/*Un role R est valide que s'il existe un identificateur de role associe a ce role*/
+role(R) :- rname(R).
 
 /*Grammaire*/
 concept(not(C)) :- concept(C).
@@ -27,3 +51,47 @@ concept(some(R, C)) :- role(R), concept(C).
 concept(all(R, C)) :- role(R), concept(C).
 
 
+/*---- Verification syntaxique de la TBox et ABox ----*/
+
+/*Construction des listes de TBox et ABox*/
+/*TBox contient les definitions
+Notons que dans Tbox on a ne represente pas de relations de subsumption par souci de simplification (enoncé)*/
+listeTBox(TBox) :- setof((Concept_g, Concept_d), equiv(Concept_g, Concept_d), TBox).
+/*ABoxCon contient les assertions de concepts*/
+listeABoxConcept(ABoxConcept) :- setof((Instance, Concept), inst(Instance, Concept), ABoxConcept).
+/*ABoxRole contient les assertions de roles*/
+listeABoxRole(ABoxRole) :- setof((Instance1, Instance2, Role), instR(Instance1, Instance2, Role), ABoxRole).
+
+
+/*--- Verifier la syntaxe de ABox ---*/
+
+/**Cas1 : ABoxConcept contient les assertions de concepts*/
+verifierInst(Instance, Concept) :- instance(Instance), cnamena(Concept).
+verifierInst(Instance, Concept) :- instance(Instance), cnamea(Concept).
+/*Cas de base ou la ABox est vide*/
+syntaxeABoxConcept([]).
+/*Verifier que chaque element de la ABoxConcept est une assertion de concept valide */
+syntaxeABoxConcept([(Instance, Concept) | L]) :- verifierInst(Instance, Concept) , 
+                                                    syntaxeABoxConcept(L).
+
+/**Cas2 : ABoxRole contient les assertions de roles*/
+verifierInstR(Instance1, Instance2, Role) :- instance(Instance1), instance(Instance2), role(Role).
+/*Cas de base ou la ABox est vide*/
+syntaxeABoxRole([]).
+/*Verifier que chaque element de la ABoxRole est une assertion de role valide */
+syntaxeABoxRole([(Instance1, Instance2, Role)] | L) :- verifierInstR(Instance1, Instance2, Role), 
+                                                        syntaxeABoxRole(L).
+
+/*Il faut que les 2 cas soient verifies*/
+syntaxeABox(ABoxConcept, ABoxRole) :- syntaxeABoxConcept(ABoxConcept), syntaxeABoxRole(ABoxRole).
+
+/*--- Verifier la syntaxe de TBox ---*/
+
+verifierEquiv(Concept_g, Concept_d) :- cnamena(Concept_g), concept(Concept_d).
+/*Cas de base ou la TBox est vide*/
+syntaxeTBox([]).
+/*Verifier que chaque element de la TBox est une equivalence (definition) valide */
+syntaxeTBox([(Concept_g, Concept_d) | L]) :- verifierEquiv(Concept_g, Concept_d), SyntaxeTBox(L).
+
+/*--- Verifier la syntaxe generale ---*/
+syntaxe(TBox, ABoxConcept, ABoxRole) :- syntaxeTBox(TBox), syntaxeABox(ABoxConcept, ABoxRole).
