a616fc091a6e19d80570c46ba579ba56b3d50dec
Iordache Paul-Tiberiu <123265734+PaulTiberiu@users.noreply.github.com>
2023-12-01 15:37:15 +0100

    Everything works

--- partie1.pl
+++ partie1.pl
@@ -1,4 +1,3 @@
-
 /*1.---------------------------- Verification de la correction semantique ----------------------------*/
 
 
@@ -16,12 +15,12 @@ semantique :- setof(Ca, cnamea(Ca), ListeConceptsAtomiques),
 /* Verifier le cas de base la liste vide est unique ou qu'on test sur un seul element*/
 unique([]).
 /*sinon, faire unique([X])*/
-unique(X | []).
+unique(_ | []).
 /* Verifier l'unicite des elements pour la tete de la liste*/
 unique([X | L]) :-
     /* Verifier que X n'est pas dans L*/
-    \+ member(X, L), 
-    /*reccursion*/
+    not(member(X, L)), 
+    /*recursion*/
     unique(L).
 
 
@@ -31,6 +30,7 @@ unique([X | L]) :-
 
 /*---- Concepts atomiques et non atomiques ----*/
 /*Un concept est valide que s'il est atomique ou non atomique*/
+:- discontiguous syntaxeConcept/1.
 syntaxeConcept(X) :- cnamea(X).
 syntaxeConcept(X) :- cnamena(X).
 /*Ajouter les identificateurs anything et nothing qui correspondent respectivement a ⊤ et ⊥*/
@@ -69,7 +69,7 @@ listeABoxRole(ABoxRole) :- setof((Instance1, Instance2, Role), instR(Instance1, 
 
 
 
-
+/*ABOX AFFIHCE SANS S'ARRETER*/
 /*--- Verifier la syntaxe de ABox ---*/
 
 /**Cas1 : ABoxConcept contient les assertions de concepts*/
@@ -86,11 +86,15 @@ verifierInstR(Instance1, Instance2, Role) :- instance(Instance1), instance(Insta
 /*Cas de base ou la ABox est vide*/
 syntaxeABoxRole([]).
 /*Verifier que chaque element de la ABoxRole est une assertion de role valide */
-syntaxeABoxRole([(Instance1, Instance2, Role)] | L) :- verifierInstR(Instance1, Instance2, Role), 
-                                                        syntaxeABoxRole(L).
+syntaxeABoxRole([(Instance1, Instance2, Role) | L]) :- 
+    verifierInstR(Instance1, Instance2, Role), 
+    syntaxeABoxRole(L).
+
 
 /*Il faut que les 2 cas soient verifies*/
-syntaxeABox(ABoxConcept, ABoxRole) :- syntaxeABoxConcept(ABoxConcept), syntaxeABoxRole(ABoxRole).
+syntaxeABox(ABoxConcept, ABoxRole) :- syntaxeABoxConcept(ABoxConcept), write("OK ABoxConcept"), nl,
+                                    syntaxeABoxRole(ABoxRole), write("OK ABoxRole"), nl.
+
 
 
 
@@ -109,11 +113,18 @@ syntaxeTBox([(Concept_g, Def) | L]) :- verifierEquiv(Concept_g, Def), syntaxeTBo
 
 /*--- Verifier la syntaxe generale ---*/
 
-syntaxe(TBox, ABoxConcept, ABoxRole) :- syntaxeTBox(TBox), syntaxeABox(ABoxConcept, ABoxRole).
+syntaxe(TBox, ABoxConcept, ABoxRole) :- syntaxeTBox(TBox),write("TBOX SYNTAXE OK"),nl,
+                                        syntaxeABox(ABoxConcept, ABoxRole), write("ABOX SYNTAXE OK"), nl.
 
 
-/*-------------------- concept : Verification de la correction semantique de la correction syntxique --------------------*/
+/*-------------------- concept : Verification de la correction semantique de la correction syntaxique --------------------*/
 /*concept est a true si la semantique est correcte et que la syntaxe aussi*/
+/*concept :- semantique,
+            listeTBox(TBox), write("TBox = "), write(TBox), nl,
+            listeABoxConcept(ABoxConcept), write("ABoxC = "), write(ABoxConcept), nl,
+            listeABoxRole(ABoxRole), write("ABoxR = "), write(ABoxRole), nl,
+            syntaxe(TBox, ABoxConcept, ABoxRole).*/
+
 concept :- semantique,
             listeTBox(TBox),
             listeABoxConcept(ABoxConcept),
@@ -121,10 +132,12 @@ concept :- semantique,
             syntaxe(TBox, ABoxConcept, ABoxRole).
 
 
+
 /*3.---------------------------- Verification l'autoreference ----------------------------*/
 
 /*Cas ou la definition est un concept atomique: rien à verifier on est sur qu'il n'y a pas de cycle*/
-pasAutoRef_element(Concept, Def) :- cnamea(Def). 
+/*_ ici est un Concept*/
+pasAutoRef_element(_, Def) :- cnamea(Def). 
 
 /*Cas ou la definition est un concept non atomique: 1. verifier qu'il n'est pas unifiable avec le concept gauche et 2. recuperer la definition de la definition puis 3. 
 Appliquer la recursivite pour s'assurer qu'il n'y a pas de cycle en partant cette fois de la definition de la definition*/
@@ -135,8 +148,9 @@ pasAutoRef_element(Concept, Def) :- cnamena(Def), Concept \= Def, equiv(Def, Def
 pasAutoRef_element(Concept, and(Def1, Def2)) :- pasAutoRef_element(Concept, Def1), pasAutoRef_element(Concept, Def2).
 pasAutoRef_element(Concept, or(Def1, Def2)) :- pasAutoRef_element(Concept, Def1), pasAutoRef_element(Concept, Def2).
 pasAutoRef_element(Concept, not(Def1)) :- pasAutoRef_element(Concept, Def1).
-pasAutoRef_element(Concept, some(R, C)) :- pasAutoRef_element(Concept, C).
-pasAutoRef_element(Concept, all(R, C)) :- pasAutoRef_element(Concept, C).
+/*_ ici est un role*/
+pasAutoRef_element(Concept, some(_, C)) :- pasAutoRef_element(Concept, C).
+pasAutoRef_element(Concept, all(_, C)) :- pasAutoRef_element(Concept, C).
 
 /*Cas de base*/
 pasAutoRef_list([]). 
@@ -152,7 +166,6 @@ pasAutoRef_list([(Concept, Def)| L]) :- pasAutoRef_element(Concept, Def), pasAut
 /*test : N'OUBLIER PAS D'AJOUTER DES WRITE POUR VOIR LE RESULTAT DANS LE TERMINAL*/
 pas_autoref :- listeTBox(TBox), pasAutoRef_list(TBox).
 /*pasAutoRef_fichier() :- listeTBox(TBox), pasAutoRef_list(TBox)...--------------------------------------------------------------------*/
-
 
 
 
@@ -181,8 +194,13 @@ remplacer(Concept, Def) :- cnamena(Concept), equiv(Concept, Def2), remplacer(Def
 remplacer(not(Concept), not(Def)) :- remplacer(Concept, Def).
 remplacer(and(Concept1, Concept2), and(Def1, Def2)) :- remplacer(Concept1, Def1), remplacer(Concept2, Def2).
 remplacer(or(Concept1, Concept2), or(Def1, Def2)) :- remplacer(Concept1, Def1), remplacer(Concept2, Def2).
-remplacer(all(Role1, Concept), all(Role2,Def)) :- Role1 == Role2, remplacer(Concept,Def). 
-remplacer(some(Role1, Concept), some(Role2,Def)) :- Role1 == Role2, remplacer(Concept,Def). 
+remplacer(all(Role1, Concept), all(Role2,Def)) :- Role1 = Role2, remplacer(Concept,Def). 
+remplacer(some(Role1, Concept), some(Role2,Def)) :- Role1 = Role2, remplacer(Concept,Def). 
+
+/*
+remplacer(all(Role1, Concept), all(Role1,Def)) :- remplacer(Concept,Def). 
+remplacer(some(Role1, Concept), some(Role1,Def)) :- remplacer(Concept,Def).
+*/
 
 /*Cas de base*/
 remplacer_liste_TBox([], []).
@@ -196,7 +214,10 @@ remplacer_liste_TBox([(C1, Def1) | L1], [(C1, Def2) | L2]) :- remplacer(Def1, De
                                                             remplacer_liste_TBox(L1, L2).
 
 /*traitement_Tbox retourne true si elle est appliquee a la TBox ecrite sous forme normale negative*/
-traitement_TBox(nnf_TBox) :- listeTBox(TBox), remplacer_liste_TBox(TBox, nnf_TBox).
+traitement_TBox(NNF_TBox) :- listeTBox(TBox), write("OK listeTBox"),nl,
+                            write("liste_TBox = "), write(TBox),nl,
+                            remplacer_liste_TBox(TBox, NNF_TBox), write("OK remplacerlisteTBox"),nl,
+                            write("liste_TBox sous NNF = "), write(NNF_TBox),nl.
 
 
 
@@ -204,7 +225,8 @@ traitement_TBox(nnf_TBox) :- listeTBox(TBox), remplacer_liste_TBox(TBox, nnf_TBo
 
 /* --- Verifier si un concept (definit pour une certaine instance dans la ABox) est dans la TBox finale (simplifiee et mise sous nnf) --- */
 /*Cas ou le concept est en tete de liste de la TBox*/
-verifier_concept_dans_TBox(Concept, Def, [(C1, Def1) | L] ) :- Concept = C1, Def = Def1.
+/*_ ici c'est la suite d'une liste L*/
+verifier_concept_dans_TBox(Concept, Def, [(C1, Def1) | _] ) :- Concept = C1, Def = Def1.
 /*Cas ou le concept n'est pas en tete de liste de la TBox, soit appliquer une recursiviter sur les elements restants de la liste */
 verifier_concept_dans_TBox(Concept, Def, [(C1, Def1) | L] ) :- Concept \= C1, Def = Def1, verifier_concept_dans_TBox(Concept, Def, L).
 /*Note : verifier_concept_dans_TBox retourne true si Def est bien la definition simplifiee est mise sous forme normale negative de Concept, qu'on retrouve dans nnf_TBox*/
@@ -220,34 +242,24 @@ remplacer_liste_ABox([(Inst, Concept) | L1], [(Inst, Concept) | L2]) :- cnamea(C
 remplacer Concept par Def qui est sa definition simplifiee et mise sous nnf (qu'on recuperer à partir de la TBox traitee) 
 puis passer au prochain element de la liste ABox*/
 remplacer_liste_ABox([(Inst, Concept) | L1], [(Inst, Def) | L2]) :- cnamena(Concept), 
-                                                                    traitement_TBox(nnf_TBox),
-                                                                    verifier_concept_dans_TBox(Concept, Def, nnf_TBox),
+                                                                    traitement_TBox(NNF_TBox),
+                                                                    verifier_concept_dans_TBox(Concept, Def, NNF_TBox),
                                                                     remplacer_liste_ABox(L1, L2).
 
 /*traitement_ABox retourne true si elle est appliquee a la ABox ecrite sous forme normale negative*/
-traitement_ABox(nnf_ABox) :- listeABoxConcept(ABoxConcept),  remplacer_liste_ABox(ABoxConcept, nnf_ABox).
+traitement_ABox(NNF_ABox) :- listeABoxConcept(ABoxConcept),write("OK listeABoxConcept"),nl,
+                            write("ABoxConcept = "), write(ABoxConcept),nl,
+                            remplacer_liste_ABox(ABoxConcept, NNF_ABox),write("OK remplacerlisteABox"),nl,
+                            write("ABoxConcept sous NNF = "), write(NNF_ABox),nl.
 /*Note : pour verifier la coherence d'une ABox il est inutile de traiter le cas des assertions de roles car un role ne peut etre ni simplifie ni mis sous nnf*/
 
 /*-------------------- partie1 : Verification de la coherence de toute la premiere partie --------------------*/
 
 
-premiere_etape(TBox, Abi, Abr) :-  (concept, write("La correction semantique et syntaxique sont verifiees"), nl; nl,
-                                            write("La correction semantique et syntaxique ne sont pas verifiees"), nl, fail).
 
-
-/*
-premiere_etape(TBox, Abi, Abr) :-  (concept, write("La correction semantique et syntaxique sont verifiees"), nl; nl,
-                                            write("La correction semantique et syntaxique ne sont pas verifiees"), nl, fail),
-                                            (pas_autoref, write("Il n'y a pas des definitions des concepts circulaires"), nl; nl,
-                                            write("Il existe au moins une definition de concept circulaire"), nl, fail),
-                                            traitement_TBox(TBox),
-                                            traitement_ABox(Abi),
-                                            listeABoxRole(Abr).
-*/
-/*
-premiere_etape(TBox, Abi, Abr) :- concept(),
-                                pas_autoref(),
+premiere_etape(TBox, Abi, Abr) :- concept, write("La correction semantique et syntaxique sont verifies"),nl,
+                                pas_autoref, write("Pas d'autoreference"), nl,
                                 traitement_TBox(TBox),
                                 traitement_ABox(Abi),
-                                listeABoxRole(Abr).
-*/
+                                listeABoxRole(Abr),
+                                write("listeABoxRole = "), write(Abr),nl.
