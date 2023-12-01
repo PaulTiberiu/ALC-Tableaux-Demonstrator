7a3a159d0cf54a329abfa7ad058e74e754bb2cbd
Iordache Paul-Tiberiu <123265734+PaulTiberiu@users.noreply.github.com>
2023-12-01 15:36:38 +0100

    Delete programmebis.pl

--- programmebis.pl
+++
@@ -1,4 +0,0 @@
-programme :- load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/tools.pl'),
-            load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/partie1bis.pl'),
-            load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/TBox_ABox.pl'), 
-            premiere_etape(TBox, Abi, Abr).
