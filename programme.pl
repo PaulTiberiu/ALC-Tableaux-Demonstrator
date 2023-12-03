83abf5bba8ef57201e53ad4fa7bda718e14daf83
Iordache Paul-Tiberiu <123265734+PaulTiberiu@users.noreply.github.com>
2023-12-03 13:31:07 +0100

    Update programme.pl

--- programme.pl
+++ programme.pl
@@ -1,4 +1,6 @@
 programme :- load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/tools.pl'),
             load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/partie1.pl'),
             load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/TBox_ABox.pl'), 
-            premiere_etape(TBox, Abi, Abr).
+            load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/partie2.pl'),
+            premiere_etape(TBox, Abi, Abr),
+            deuxieme_etape(Abi, Abi1, TBox).
