bf18c24dae1456e1d423e65cb3c747aa39a12c80
Iordache Paul-Tiberiu <123265734+PaulTiberiu@users.noreply.github.com>
2023-12-26 09:53:58 +0100

    Delete programme.pl

--- programme.pl
+++
@@ -1,8 +0,0 @@
-programme :- load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/tools.pl'),
-            load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/partie1.pl'),
-            load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/TBox_ABox.pl'), 
-            load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/partie2.pl'),
-            load_files('C:/Users/iorda/Desktop/Master/S1/LRC/Projet/partie3.pl'),
-            premiere_etape(TBox, Abi, Abr),
-            deuxieme_etape(Abi, Abi1, TBox),
-            troisieme_etape(Abi, Abr).
